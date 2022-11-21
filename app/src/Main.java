8662f76870e279914edfcffbbc08aae88b4c0957
Tomek <soplinho@wp.pl>
2022-11-21 15:26:33 +0100

    Liczenie pierwiastka

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
         while (true) {
             System.out.println("Podaj liczbę do liczenia pierwiastka");
+            Scanner scanner = new Scanner(System.in);
+            int liczba = scanner.nextInt();
+            System.out.println(Math.sqrt(liczba));
         }
     }
 }
